ad33bf8b21a646f2f9b7359449f3d19b60323c52
Abhishek Dehariya <148337229+Abhishek-Dehariya@users.noreply.github.com>
2025-07-11 11:17:17 +0530

    Namaste react first part have been completed

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-const heading = React.createElement("h1",
-    {id:"heading", xyz:"abc"
+// const heading = React.createElement("h1",
+//     {id:"heading", xyz:"abc"
 
-    }, "Hello World from React!");
+//     }, "Hello World from React!");
 
-    console.log(heading)
+//     console.log(heading)
+
+{/* <div id="perent">
+    <div id ="child">
+        <h1>I am H1</h1>
+         <h1>I am H1</h1>
+    </div>
+    <div id ="child2">
+        <h1>I am H1</h1>
+         <h1>I am H1</h1>
+    </div>
+</div> */}
+
+const perent = React.createElement("div",{id:"perent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag"),
+     React.createElement("h1",{},"I am h1 tag"),
+    ]),
+      React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+     React.createElement("h1",{},"I am h1 tag"),
+    ]),  
+]);
+
+console.log(perent);// object
+
     
     const root = ReactDOM.createRoot(document.getElementById("root"));
     
-    root.render(heading);
+    root.render(perent);
